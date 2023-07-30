d81fe0a3712894c8f49a6cbc2fea1c63cafb2086
Logan Cox <mail@logan-cox.com>
2023-07-30 11:54:14 +0100

    Refactor how subnets are created and add ability to bind them to an AZ

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,23 @@
 locals {
+  default_region = "eu-west-1"
   main_vpc_cidr = "10.0.0.0/16"
   subnets = {
-    public_01  = "10.0.1.0/24"
-    public_02  = "10.0.2.0/24"
-    private_01 = "10.0.3.0/24"
-    private_02 = "10.0.4.0/24"
+    public_01  = {
+        az   = "a", 
+        cidr = cidrsubnet(local.main_vpc_cidr, 8, 0)
+    }
+    public_02  = {
+        az   = "b", 
+        cidr = cidrsubnet(local.main_vpc_cidr, 8, 1)
+    }
+    private_01 = {
+        az   = "a", 
+        cidr = cidrsubnet(local.main_vpc_cidr, 8, 2)
+    }
+    private_02 = {
+        az   = "b", 
+        cidr = cidrsubnet(local.main_vpc_cidr, 8, 3)
+    }
   }
 }
 
@@ -14,6 +27,9 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "main" {
   for_each = local.subnets
-  vpc_id     = aws_vpc.main.id
-  cidr_block = each.value
+  
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = each.value.cidr
+  availability_zone = "${local.default_region}${each.value.az}"
 }
+
